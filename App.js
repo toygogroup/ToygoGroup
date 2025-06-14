93fe32c6e711758d0d5059d6aef0e30fedf88267
toygogroup <ToygoGroup@gmail.com>
2025-06-14 12:55:27 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: 20 }}>
+      <h1>🚀 أهلًا وسهلًا في تويجو جروب!</h1>
+      <p>أول منصة ميتافيرس عربية تعمل من المتصفح</p>
+      <img 
+        src="https://cdn.pixabay.com/photo/2022/11/13/19/42/metaverse-7589917_1280.png" 
+        width="200"
+        alt="ميتافيرس"
+      />
+    </div>
+  );
+}
+
+export default App;
